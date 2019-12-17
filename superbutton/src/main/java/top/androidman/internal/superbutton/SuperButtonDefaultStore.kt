d22117d833d240eba8e29fb0009c35850787f500
yanjie <androidman@yeah.net>
2019-12-18 01:28:57 +0800

    准备superbutton开发

---
+++ superbutton/src/main/java/top/androidman/internal/superbutton/SuperButtonDefaultStore.kt
@@ -0,0 +1,17 @@
+package top.androidman.internal.superbutton
+
+import top.androidman.internal.DefaultStore
+
+
+/**
+ * @author         yanjie
+ * @date           2019-12-17 23:54
+ * @version        1.0
+ */
+class SuperButtonDefaultStore : DefaultStore() {
+    /**
+     * 文字内容
+     */
+    var text: CharSequence = ""
+
+}
---
+++ superbutton/src/main/java/top/androidman/internal/superbutton/SuperButtonPlasterer.kt
@@ -0,0 +1,49 @@
+package top.androidman.internal.superbutton
+
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatTextView
+import top.androidman.SuperLinearLayout
+import top.androidman.internal.DefaultStore
+import top.androidman.internal.Plasterer
+
+
+/**
+ * @author         yanjie
+ * @date           2019-12-17 23:50
+ * @version        1.0
+ */
+
+class SuperButtonPlasterer(linearLayout: LinearLayout, valueStore: SuperButtonDefaultStore) : Plasterer(linearLayout, valueStore) {
+
+    /**
+     * 粉刷图纸
+     */
+    private var globalStore: SuperButtonDefaultStore = valueStore
+    /**
+     * 粉刷对象
+     */
+    private var paintObject: LinearLayout = linearLayout
+
+
+    override fun startPaint() {
+        //1.全部内容居中
+        paintObject.gravity = Gravity.CENTER
+        //2.创建文字组件
+        val textContent = AppCompatTextView(paintObject.context)
+        //3.设置文字大小
+        textContent.setTextSize(TypedValue.COMPLEX_UNIT_PX, 50f)
+        //4.设置文字颜色
+        textContent.setTextColor(Color.RED)
+        //5.设置文字内容
+        textContent.text = globalStore.text
+
+        val layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        paintObject.addView(textContent, layoutParams)
+
+    }
+
+}
